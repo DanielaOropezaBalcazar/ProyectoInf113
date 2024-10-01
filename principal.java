35bafe477d2d7d67d10f3c328c7e9ac14a9ecd3e
daniela <danyoro1894@gmail.com>
2024-10-01 15:02:39 -0400

    Primera Version

---
+++ principal.java
@@ -0,0 +1,15 @@
+package proyecto_cvs;
+import proyecto_cvs.*;
+
+public class principal {
+
+	public static void main(String[] args) {
+		
+		propiedad pr2 = new propiedad();
+		pr2.setNombre("Plaza Santiago");
+		System.out.println(pr2.getNombre());
+		System.out.println(pr2.getAlquiler());
+
+	}
+
+}
---
+++ propiedad.java
@@ -0,0 +1,25 @@
+package proyecto_cvs;
+
+public class propiedad {
+	
+	private String nombre;
+	private int alquiler;
+	
+	propiedad() {}
+
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getAlquiler() {
+		return alquiler;
+	}
+	public void setAlquiler(int alquiler) {
+		this.alquiler = alquiler;
+	}
+	
+	
+}
